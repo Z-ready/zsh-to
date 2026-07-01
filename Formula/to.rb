29eaba8071de3167d47444ebe99cf859f8bdb14d
Z-Ready <zrd1316176591@gmail.com>
2026-07-01 20:48:56 +1000

    Update Homebrew formula for v1.0.2

--- Formula/to.rb
+++ Formula/to.rb
@@ -1,9 +1,9 @@
 class To < Formula
   desc "Exploratory zsh directory jumper"
   homepage "https://github.com/Z-ready/zsh-to"
-  url "https://github.com/Z-ready/zsh-to/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "016c2b8b05eb98a1574c1c98bf24c6d3d45064fce4817cd777d9ef6785a1b196"
-  version "1.0.1"
+  url "https://github.com/Z-ready/zsh-to/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "2bb81bc6dcf7e4af9719a9770db977d4e72068e4381e0e1eb3b665b4ef432a7b"
+  version "1.0.2"
   license "MIT"
 
   depends_on "fd" => :recommended
